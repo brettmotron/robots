e2093fbb2f3d45ec67f1e95bf3e99132f19c42a4
Brett Sweeney <brett@simutronics.com>
2013-05-25 05:43:53 -0500

    Add cards to hand

--- Assets/Scripts/CardVisualizer.cs
+++ Assets/Scripts/CardVisualizer.cs
@@ -5,23 +5,37 @@ using System.Collections.Generic;
 public class CardVisualizer : MonoBehaviour {
 
 	public List<GameObject> cardSlots;
+	public List<GameObject> handSlots;
 	public List<CommandCard> commandCardPrefabs;
-	List<GameObject> currentCards = new List<GameObject>();
+	public List<GameObject> currentCards = new List<GameObject>();
+	public List<GameObject> currentHandCards = new List<GameObject>();
 	
-	public void SetCards(List<Robot.Command> commands) {
-		ClearCurrentCards();
-		
-		if (commands.Count != cardSlots.Count) {
-			Debug.Log("Visualizer has wrong number of cards!");
-			return;
-		}
-		
-		for (int i=0; i<cardSlots.Count; ++i) {
+	public void SetActiveCards(List<Robot.Command> commands) {
+		for (int i=0; i<commands.Count; ++i) {
 			var newCard = CreateCommandCardForCommand(commands[i]);
-			newCard.transform.parent = cardSlots[i].transform;
-			newCard.transform.localPosition = Vector3.zero;
-			currentCards.Add(newCard.gameObject);
+			if (newCard) {
+				newCard.transform.parent = cardSlots[i].transform;
+				newCard.transform.localPosition = Vector3.zero;
+				currentCards.Add(newCard.gameObject);
+			} else {
+				currentCards.Add(null);	
+			}
+			
 		}
+	}
+	
+	public void SetHandCards(List<Robot.Command> commands) {
+		for (int i=0; i<commands.Count; ++i) {
+			var newCard = CreateCommandCardForCommand(commands[i]);
+			if (newCard) {
+				newCard.transform.parent = handSlots[i].transform;
+				newCard.transform.localPosition = Vector3.zero;
+				currentHandCards.Add(newCard.gameObject);
+			} else {
+				currentHandCards.Add(null);	
+			}
+			
+		}		
 	}
 	
 	void ClearCurrentCards() {
@@ -30,16 +44,29 @@ public class CardVisualizer : MonoBehaviour {
 		}
 		
 		currentCards.Clear();
+		
+		foreach (GameObject gameObj in currentHandCards) {
+			Destroy(gameObj);	
+		}
+		
+		currentHandCards.Clear();		
 	}
 	
-	CommandCard CreateCommandCardForCommand(Robot.Command command) {
+	public void UpdateVisualizations(List<Robot.Command> activeCommands, List<Robot.Command> handCommands) {
+		ClearCurrentCards();
+		SetActiveCards(activeCommands);
+		SetHandCards(handCommands);
+	}
+	
+	
+	public CommandCard CreateCommandCardForCommand(Robot.Command command) {
 		foreach (CommandCard card in commandCardPrefabs) {
 			if (card.command == command) {
 				return (CommandCard)GameObject.Instantiate(card);	
 			}
 		}
 		
-		Debug.Log("No appropriate card found!");
+		Debug.Log("No appropriate card found for command: " + command);
 		return null;
 	}
 	
--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RobotController : MonoBehaviour {
 	
@@ -7,21 +8,86 @@ public class RobotController : MonoBehaviour {
 	public CommandDeck commandDeck;
 	public CardVisualizer visualizer;
 	
+	public List<Robot.Command> commandHand;
+	public List<Robot.Command> activeCommands;
+	
 	void Start() {
 		commandDeck = new CommandDeck();
 		commandDeck.CreateRandomDeck(60);
+		
+		commandHand = new List<Robot.Command>();
+		activeCommands = new List<Robot.Command>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyUp(KeyCode.N)) {
 			//Generate Command Hand
-			commandDeck.Shuffle();
-			var commandHand = commandDeck.DrawCards(5);
-			foreach (Robot.Command command in commandHand) {
+			DrawNewHand();
+		}
+		
+		if (Input.GetKeyUp(KeyCode.Return)) {
+			//Finalize active cards
+			if (activeCommands.Count != 5) {
+				Debug.Log("Incomplete command set.");
+			}
+			
+			foreach (Robot.Command command in activeCommands) {
 				robotToControl.QueueCommand(command);	
 			}
-			visualizer.SetCards(commandHand);
 		}
+		
+		if (Input.GetKeyUp(KeyCode.Alpha1)) {
+			AddCardToActiveCards(0);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha2)) {
+			AddCardToActiveCards(1);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha3)) {
+			AddCardToActiveCards(2);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha4)) {
+			AddCardToActiveCards(3);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha5)) {
+			AddCardToActiveCards(4);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha6)) {
+			AddCardToActiveCards(5);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha7)) {
+			AddCardToActiveCards(6);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha8)) {
+			AddCardToActiveCards(7);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha9)) {
+			AddCardToActiveCards(8);	
+		}
+		if (Input.GetKeyUp(KeyCode.Alpha0)) {
+			AddCardToActiveCards(9);	
+		}		
+	}
+	
+	void AddCardToActiveCards(int index) {
+		if (activeCommands.Count >= 5) {
+			Debug.Log("Too many commands!");
+			return;
+		}
+		
+		
+		var temp = commandHand[index];
+		activeCommands.Add(temp);
+		commandHand[index] = Robot.Command.None;
+		
+		visualizer.UpdateVisualizations(activeCommands, commandHand);
+	}
+	
+	void DrawNewHand() {
+		commandDeck.Shuffle();
+		commandHand = commandDeck.DrawCards(10);
+		activeCommands.Clear();
+		
+		visualizer.UpdateVisualizations(activeCommands, commandHand);
 	}
 }
